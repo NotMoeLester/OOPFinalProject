ed955ab8995444d27e56962550806733cde3953f
Lansxz1 <lancebulan1928@gmail.com>
2025-12-04 17:46:34 +0800

    Updated messageboxes on Validator, SignUp, and Login

--- Project/StudentUserData/Validator.cs
+++ Project/StudentUserData/Validator.cs
@@ -9,15 +9,15 @@ namespace Project {
     internal class Validator {
         public static bool Password(string password) {
             if (string.IsNullOrEmpty(password) || password == "Enter Password") {
-                MessageBox.Show("Password Field Empty");
+                MessageBox.Show("Password Field Empty", "Please try again!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
             if (password.Length < 8) {
-                MessageBox.Show("Passsword must me be atleast 8 characters");
+                MessageBox.Show("Passsword must me be atleast 8 characters", "Please try again!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
             if (!password.Any(char.IsLetter) || !password.Any(char.IsDigit) || !password.Any(char.IsUpper) || !password.Any(char.IsLower) || !password.Any(char.IsPunctuation)) {
-                MessageBox.Show("Passsword must contain atleast one Uppercase character|  Lowercase character | Number | Symbol");
+                MessageBox.Show("Passsword must contain atleast one Uppercase character|  Lowercase character | Number | Symbol", "Please try again!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
             return true;
@@ -29,11 +29,11 @@ namespace Project {
                 return false;
             }
             if (!(email.Contains('@') && email.Split('@')[1].Contains('.') && email.All(c => char.IsLetterOrDigit(c) || c == '@' || c == '.' || c == '-' || c == '_'))) {
-                MessageBox.Show("Invalid email syntax");
+                MessageBox.Show("Invalid email syntax", "Please try again!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
             if (!email.StartsWith("hed-") || !email.EndsWith("@smu.edu.ph")) {
-                MessageBox.Show("Use your HED account");
+                MessageBox.Show("Use your HED account", "Please try again!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
             return true;
