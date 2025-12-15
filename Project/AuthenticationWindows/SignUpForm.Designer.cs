c329eff7ec910a2dba67c405133f113488b49d17
NotMoeLester <itsmark1567@gmail.com>
2025-12-15 14:27:18 +0800

    UI changes

--- Project/AuthenticationWindows/SignUpForm.Designer.cs
+++ Project/AuthenticationWindows/SignUpForm.Designer.cs
@@ -22,7 +22,8 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUpForm));
             LogIn = new Label();
             TextBoxEmailSignUp = new TextBox();
@@ -41,7 +42,6 @@
             panel2 = new Panel();
             panel3 = new Panel();
             pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             pictureBox4 = new PictureBox();
             pictureBox3 = new PictureBox();
             pictureBox5 = new PictureBox();
@@ -49,7 +49,6 @@
             panel1.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
@@ -59,7 +58,7 @@
             // LogIn
             // 
             LogIn.AutoSize = true;
-            LogIn.Location = new Point(364, 254);
+            LogIn.Location = new Point(364, 239);
             LogIn.Name = "LogIn";
             LogIn.Size = new Size(0, 15);
             LogIn.TabIndex = 17;
@@ -83,7 +82,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Navy;
-            label1.Location = new Point(286, 82);
+            label1.Location = new Point(286, 67);
             label1.Name = "label1";
             label1.Size = new Size(159, 32);
             label1.TabIndex = 14;
@@ -94,7 +93,7 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 6.75F);
             label2.ForeColor = Color.Navy;
-            label2.Location = new Point(286, 249);
+            label2.Location = new Point(286, 234);
             label2.Name = "label2";
             label2.Size = new Size(41, 12);
             label2.TabIndex = 21;
@@ -105,7 +104,7 @@
             LabelPassword.AutoSize = true;
             LabelPassword.Font = new Font("Segoe UI", 6.75F);
             LabelPassword.ForeColor = Color.Navy;
-            LabelPassword.Location = new Point(286, 186);
+            LabelPassword.Location = new Point(286, 171);
             LabelPassword.Name = "LabelPassword";
             LabelPassword.Size = new Size(47, 12);
             LabelPassword.TabIndex = 13;
@@ -130,7 +129,7 @@
             LabelName.AutoSize = true;
             LabelName.Font = new Font("Segoe UI", 6.75F);
             LabelName.ForeColor = Color.Navy;
-            LabelName.Location = new Point(286, 124);
+            LabelName.Location = new Point(286, 109);
             LabelName.Name = "LabelName";
             LabelName.Size = new Size(31, 12);
             LabelName.TabIndex = 12;
@@ -140,7 +139,7 @@
             // 
             LinkLabelLogin.AutoSize = true;
             LinkLabelLogin.LinkColor = Color.Gray;
-            LinkLabelLogin.Location = new Point(392, 361);
+            LinkLabelLogin.Location = new Point(392, 346);
             LinkLabelLogin.Name = "LinkLabelLogin";
             LinkLabelLogin.Size = new Size(124, 15);
             LinkLabelLogin.TabIndex = 23;
@@ -154,7 +153,7 @@
             ButtonSignUp.FlatStyle = FlatStyle.Flat;
             ButtonSignUp.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             ButtonSignUp.ForeColor = SystemColors.ControlLightLight;
-            ButtonSignUp.Location = new Point(286, 319);
+            ButtonSignUp.Location = new Point(286, 304);
             ButtonSignUp.Name = "ButtonSignUp";
             ButtonSignUp.Size = new Size(230, 33);
             ButtonSignUp.TabIndex = 11;
@@ -167,7 +166,7 @@
             TextBoxPasswordSignUp.BorderStyle = BorderStyle.None;
             TextBoxPasswordSignUp.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
             TextBoxPasswordSignUp.ForeColor = Color.Gray;
-            TextBoxPasswordSignUp.Location = new Point(289, 205);
+            TextBoxPasswordSignUp.Location = new Point(289, 190);
             TextBoxPasswordSignUp.Name = "TextBoxPasswordSignUp";
             TextBoxPasswordSignUp.Size = new Size(224, 16);
             TextBoxPasswordSignUp.TabIndex = 16;
@@ -180,7 +179,7 @@
             // 
             LabelEmailValidation.AutoSize = true;
             LabelEmailValidation.ForeColor = Color.Red;
-            LabelEmailValidation.Location = new Point(216, 165);
+            LabelEmailValidation.Location = new Point(216, 150);
             LabelEmailValidation.MinimumSize = new Size(300, 0);
             LabelEmailValidation.Name = "LabelEmailValidation";
             LabelEmailValidation.Size = new Size(300, 15);
@@ -191,7 +190,7 @@
             // 
             LabelPasswordValidation.AutoSize = true;
             LabelPasswordValidation.ForeColor = Color.Red;
-            LabelPasswordValidation.Location = new Point(216, 227);
+            LabelPasswordValidation.Location = new Point(216, 212);
             LabelPasswordValidation.MinimumSize = new Size(300, 0);
             LabelPasswordValidation.Name = "LabelPasswordValidation";
             LabelPasswordValidation.Size = new Size(300, 15);
@@ -202,7 +201,7 @@
             // 
             LabelConfirmPasswordValidation.AutoSize = true;
             LabelConfirmPasswordValidation.ForeColor = Color.Red;
-            LabelConfirmPasswordValidation.Location = new Point(217, 289);
+            LabelConfirmPasswordValidation.Location = new Point(217, 274);
             LabelConfirmPasswordValidation.MinimumSize = new Size(300, 0);
             LabelConfirmPasswordValidation.Name = "LabelConfirmPasswordValidation";
             LabelConfirmPasswordValidation.Size = new Size(300, 15);
@@ -214,7 +213,7 @@
             panel1.BackColor = Color.White;
             panel1.BorderStyle = BorderStyle.FixedSingle;
             panel1.Controls.Add(TextBoxEmailSignUp);
-            panel1.Location = new Point(286, 139);
+            panel1.Location = new Point(286, 124);
             panel1.Name = "panel1";
             panel1.Size = new Size(230, 23);
             panel1.TabIndex = 27;
@@ -223,7 +222,7 @@
             // 
             panel2.BackColor = Color.White;
             panel2.BorderStyle = BorderStyle.FixedSingle;
-            panel2.Location = new Point(286, 201);
+            panel2.Location = new Point(286, 186);
             panel2.Name = "panel2";
             panel2.Size = new Size(230, 23);
             panel2.TabIndex = 28;
@@ -233,7 +232,7 @@
             panel3.BackColor = Color.White;
             panel3.BorderStyle = BorderStyle.FixedSingle;
             panel3.Controls.Add(TextBoxConfirmPasswordSignUp);
-            panel3.Location = new Point(286, 263);
+            panel3.Location = new Point(286, 248);
             panel3.Name = "panel3";
             panel3.Size = new Size(230, 23);
             panel3.TabIndex = 29;
@@ -242,28 +241,17 @@
             // 
             pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
             pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox1.Location = new Point(152, 55);
+            pictureBox1.Location = new Point(152, 40);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(500, 403);
             pictureBox1.TabIndex = 30;
             pictureBox1.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            pictureBox2.BackColor = Color.White;
-            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
-            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox2.Location = new Point(-165, 0);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(981, 50);
-            pictureBox2.TabIndex = 31;
-            pictureBox2.TabStop = false;
-            // 
             // pictureBox4
             // 
             pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
             pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox4.Location = new Point(724, 384);
+            pictureBox4.Location = new Point(724, 388);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(64, 66);
             pictureBox4.TabIndex = 32;
@@ -274,7 +262,7 @@
             pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
             pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
             pictureBox3.InitialImage = (Image)resources.GetObject("pictureBox3.InitialImage");
-            pictureBox3.Location = new Point(6, 47);
+            pictureBox3.Location = new Point(12, 0);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(64, 66);
             pictureBox3.TabIndex = 33;
@@ -284,7 +272,7 @@
             // 
             pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
             pictureBox5.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox5.Location = new Point(668, 35);
+            pictureBox5.Location = new Point(668, 20);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(175, 136);
             pictureBox5.TabIndex = 34;
@@ -294,7 +282,7 @@
             // 
             pictureBox6.BackgroundImage = (Image)resources.GetObject("pictureBox6.BackgroundImage");
             pictureBox6.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox6.Location = new Point(-10, 227);
+            pictureBox6.Location = new Point(-10, 212);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(175, 136);
             pictureBox6.TabIndex = 35;
@@ -309,7 +297,6 @@
             Controls.Add(pictureBox6);
             Controls.Add(pictureBox5);
             Controls.Add(pictureBox4);
-            Controls.Add(pictureBox2);
             Controls.Add(LabelConfirmPasswordValidation);
             Controls.Add(LogIn);
             Controls.Add(TextBoxPasswordSignUp);
@@ -335,7 +322,6 @@
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
@@ -362,7 +348,6 @@
         private Panel panel2;
         private Panel panel3;
         private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
         private PictureBox pictureBox4;
         private PictureBox pictureBox3;
         private PictureBox pictureBox5;
